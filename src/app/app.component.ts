327746ae302d3888754291b711265e8275327f9c
Alessandro Ciccarone <ale70029@gmail.com>
2025-04-10 00:13:20 +0200

    added template components, a toolbar with links and the router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,RouterLink,
+    CommonModule,
+    MatToolbarModule,MatButtonModule, MatIconModule
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngPortfolio';
+  title = 'Portfolio';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero/hero.component';
+import { AboutComponent } from './about/about/about.component';
+import { ExperienceComponent } from './experience/experience/experience.component';
+import { ProjectsComponent } from './projects/projects/projects.component';
+import { ContactsComponent } from './contacts/contacts/contacts.component';
+import { SkillsComponent } from './skills/skills/skills.component';
+import { NotFoundComponent } from './notFound/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HeroComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contacts', component: ContactsComponent },
+
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: NotFoundComponent },
+];
