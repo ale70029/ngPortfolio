68a5bd477226fe9e613bce4f4c0ef796bde647b1
Alessandro Ciccarone <ale70029@gmail.com>
2025-04-13 18:27:46 +0200

    added skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { AsyncPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [NgFor, AsyncPipe],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit{
+
+  constructor(private dataService:DataService){}
+
+  skillsData$:any
+
+  ngOnInit(): void {
+   this.skillsData$ = this.dataService.loadJson(`data/${this.dataService.lang}/skills.json`)
+  }
+
 
 }
