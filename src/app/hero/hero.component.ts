0147b9612789124466b6733a9b2bc35ad425fa9c
Alessandro Ciccarone <ale70029@gmail.com>
2025-04-13 16:20:11 +0200

    added curriculum link

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -14,6 +14,8 @@ export class HeroComponent{
 
   isLoading=true;
   name = `ALESSANDRO\nCICCARONE`;
+  curriculumLink = "https://github.com/ale70029/ngPortfolio/raw/bd5a3099c48e08d14810d49abf7dd660e3c697fb/public/data/CV%20Alessandro%20Ciccarone.pdf";
+  
 
   constructor(){}
 }
