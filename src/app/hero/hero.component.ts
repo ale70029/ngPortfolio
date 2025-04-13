e55858e7a8c9f1ad262c999e1f6649b5093f289e
Alessandro Ciccarone <ale70029@gmail.com>
2025-04-13 22:10:04 +0200

    moved cv button to contacts

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -19,8 +19,6 @@ export class HeroComponent{
 
   showMore=false;
   name = `ALESSANDRO\nCICCARONE`;
-  curriculumLink = "https://github.com/ale70029/ngPortfolio/raw/bd5a3099c48e08d14810d49abf7dd660e3c697fb/public/data/CV%20Alessandro%20Ciccarone.pdf";
-
   constructor(){}
 
   showMoreToggle(){
