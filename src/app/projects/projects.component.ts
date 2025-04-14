733b265c5ed39496f9ff79772914ab0437035dda
Alessandro Ciccarone <ale70029@gmail.com>
2025-04-14 17:05:17 +0200

    implemented projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AsyncPipe, KeyValuePipe, NgFor, NgIf } from '@angular/common';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [
+    NgFor,NgIf,AsyncPipe,KeyValuePipe,
+    MatCardModule, MatButtonModule,MatChipsModule
+  ],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit{
+  constructor(private dataService:DataService){}
+
+  projects$:any
+  selectedProject: any = null;
+
+  showDetails(experience: any) {
+    this.selectedProject = experience;
+  }
+
+  closeDetails() {
+    this.selectedProject = null;
+  }
+
+  ngOnInit(): void {
+    this.projects$ =this.dataService.loadJson(`data/${this.dataService.lang}/projects.json`);
+  }
+
+
 
 }
