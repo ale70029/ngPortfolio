f652b4efa785a0cdb5a1ff2d964a2ce6078cb580
Alessandro Ciccarone <ale70029@gmail.com>
2025-04-14 12:23:24 +0200

    added skills tab group

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { AsyncPipe, NgFor } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-skills',
-  imports: [NgFor, AsyncPipe],
+  imports: [NgFor, AsyncPipe,
+    MatIconModule,MatTabsModule
+  ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
@@ -13,9 +19,32 @@ export class SkillsComponent implements OnInit{
   constructor(private dataService:DataService){}
 
   skillsData$:any
+  backendSkills$:any
+  frontendSkills$:any
+  dataSkills$:any
+  ideSkills$:any
+  osSkills$:any
 
   ngOnInit(): void {
-   this.skillsData$ = this.dataService.loadJson(`data/${this.dataService.lang}/skills.json`)
+   this.skillsData$ = this.dataService.loadJson(`data/${this.dataService.lang}/skills.json`);
+   this.backendSkills$ = this.skillsData$.pipe(
+    map((skills: any[]) => skills.filter(skill => skill.area === 'Backend'))
+  );
+  this.frontendSkills$ = this.skillsData$.pipe(
+    map((skills: any[]) => skills.filter(skill => skill.area === 'Frontend'))
+  );
+
+  this.dataSkills$ = this.skillsData$.pipe(
+    map((skills: any[]) => skills.filter(skill => skill.area === 'Data'))
+  );
+
+  this.ideSkills$ = this.skillsData$.pipe(
+    map((skills: any[]) => skills.filter(skill => skill.area === 'IDE'))
+  );
+
+  this.osSkills$ = this.skillsData$.pipe(
+    map((skills: any[]) => skills.filter(skill => skill.area === 'OS'))
+  );
   }
 
 
